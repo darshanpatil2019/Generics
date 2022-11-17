29ad90e81296bf0133b376c148815be3ebb5c1b8
Darshan Patil <iampatildarsh>
2022-11-17 16:39:53 +0530

    Generic Class for Find Max.Values

--- src/FindMaxValue.java
+++ src/FindMaxValue.java
@@ -1,7 +1,17 @@
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a, b, c;
+
+    //Generic Constructor for Handling Different Type Object
+    FindMaxValue(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
 
     //compareTo method for Checking Maximum from 3 Integer Object
-    public <T extends Comparable<T>> T compareTo(T a, T b, T c) {
+    public T compareTo() {
         if (a.compareTo(b) > 0) {
             if (a.compareTo(c) > 0) {
                 return a;
@@ -19,13 +29,14 @@ public class FindMaxValue {
 
     public static void main(String[] args) {
         //Creating Generic Integer Object for Integer Comparison
-        FindMaxValue integerComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 Integer Object : " + integerComparison.compareTo(20, 50, 80));
+        FindMaxValue<Integer> integerComparison = new FindMaxValue(20, 50, 80);
+        System.out.println("Maximum from 3 Integer Object : " + integerComparison.compareTo());
         //Creating Generic Float Object for Float Comparison
-        FindMaxValue floatComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 Float Object : " + floatComparison.compareTo(20.20F, 50.50F, 80.80F));
+        FindMaxValue<Float> floatComparison = new FindMaxValue<>(20.20F, 50.50F, 80.80F);
+        System.out.println("Maximum from 3 Float Object : " + floatComparison.compareTo());
         //Creating Generic String Object for String Comparison
-        FindMaxValue stringComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 String Object : " + stringComparison.compareTo("Apple", "Peach", "Banana"));
+        FindMaxValue<String> stringComparison = new FindMaxValue<>("Apple", "Peach", "Banana");
+        System.out.println("Maximum from 3 String Object : " + stringComparison.compareTo());
     }
 }
+
