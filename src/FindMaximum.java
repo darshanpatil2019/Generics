78ccd39bead97a9c9ebe3c405416798ba2aaef10
Darshan Patil <iampatildarsh>
2022-11-17 16:02:50 +0530

    Find Max of Floats Values

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,4 +1,5 @@
 public class FindMaximum {
+    // UC2- finding maximum value of 3 Integers
     public static Integer maxInteger(Integer a, Integer b, Integer c) {
 
         if (a.compareTo(b) > 0) {
@@ -11,8 +12,24 @@ public class FindMaximum {
 
     }
 
+    // UC2- finding maximum value of 3 floats
+    public static Float maxFloat(Float A, Float B, Float C) {
+
+        if (A.compareTo(B) > 0) {
+            return A;
+        } else if (C.compareTo(B) > 0) {
+            return C;
+        } else {
+            return B;
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Maximum of 3 integers is : " + maxInteger(20, 50, 80));
+        System.out.println("Maximum of 3 Integers is : " + maxInteger(20, 50, 80));
+        System.out.println("Maximum of 3 Floats is : " + maxFloat(20.20f, 50.50f, 80.80f));
     }
 }
+
+
 
--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -2,11 +2,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestMaximum {
-        @Test
-        public void maximumNumberInteger() {
-            FindMaximum maxNum = new FindMaximum();
-            Integer maxNumInteger = maxNum.maxInteger(20,50,80);
-            Assert.assertEquals(80,maxNumInteger.intValue());
-        }
+    // UC1- testing maximum value of 3 Integers
+    @Test
+    public void maximumNumberInteger() {
+        FindMaximum maxNum = new FindMaximum();
+        Integer maxNumInteger = maxNum.maxInteger(20, 50, 80);
+        Assert.assertEquals(80, maxNumInteger.intValue());
     }
+
+    // UC2- testing maximum value of 3 floats
+    @Test
+    public void maximumNumberFloat() {
+        FindMaximum maxNumber = new FindMaximum();
+        Float maxNumFloat = maxNumber.maxFloat(20.20f, 50.50f, 80.80f);
+        Assert.assertEquals(80.00f, maxNumFloat.intValue());
+    }
+}
+
+
 
