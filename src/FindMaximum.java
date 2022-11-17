81fb26dd610f43b2d8b1e9da171d7bbe5373b876
Darshan Patil <iampatildarsh>
2022-11-17 16:10:55 +0530

    Find Max of Strings Values

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,5 +1,5 @@
 public class FindMaximum {
-    // UC2- finding maximum value of 3 Integers
+    // UC1- finding maximum value of 3 Integers
     public static Integer maxInteger(Integer a, Integer b, Integer c) {
 
         if (a.compareTo(b) > 0) {
@@ -24,10 +24,23 @@ public class FindMaximum {
         }
     }
 
+    // UC3- finding maximum value of 3 strings
+    public static String maxString(String str1, String str2, String str3) {
+
+        if (str1.compareTo(str2) > 0) {
+            return str1;
+        } else if (str3.compareTo(str2) > 0) {
+            return str3;
+        } else {
+            return str2;
+        }
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Maximum of 3 Integers is : " + maxInteger(20, 50, 80));
         System.out.println("Maximum of 3 Floats is : " + maxFloat(20.20f, 50.50f, 80.80f));
+        System.out.println("Maximum of 3 Strings is : " + maxString("Apple", "Peach", "Banana"));
     }
 }
 
